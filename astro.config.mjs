2c064b9f609cae0c03247ce94f557582d1bc8fda
ILHWANK <50158086+ILHWANK@users.noreply.github.com>
2026-02-13 18:18:14 +0900

    경로 변경 테스트

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
     site: "https://ilhwank.github.io",
-    base: "/Portfolio-Web/",
+    base: "/portfolio-web/",
     trailingSlash: "always",
 });
